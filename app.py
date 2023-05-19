06c416c6137cf8d375fa37b09ed9dee5d4a4bf9c
zty <15010972518@163.com>
2023-05-19 16:03:39 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def respond_to_query():
+    if request.method == 'POST':
+        data = request.get_json()
+        query = data['query']
+        response = 'Respond to ' + query
+    else:
+        response = 'Hello, world!'
+
+    return jsonify(response=response)
+
+if __name__ == '__main__':
+    app.run(port=3002)
